1f685fc611a0ef967a2b37ca5f57a55db2e67042
Shivang <kshivang679@gmail.com>
2025-07-10 02:25:23 +0530

    travel journal project made using react , css,js and html

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Header() {
+    return (
+        <header>
+            <img src="images/globe.png" alt="globe icon" style={{ width: "50px", height: "50px" }}/>
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
